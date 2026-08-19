d2cc6bb4f32b823fa18614b3e25c8d6001f81224
xiaoCongxyx <1293984003@qq.com>
2026-08-19 15:55:08 +0800

    chore: normalize css push

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import './assets/styles/reset.css'
+import './assets/styles/variables.css'
+import './assets/styles/global.css'
+
+
 import App from './App.vue'
 import router from './router'
 
